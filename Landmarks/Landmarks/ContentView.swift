becbf2d09c7835533423a9aff62a467dc9be7b0f
Robby Stigler <rstigler@captechconsulting.com>
2019-10-29 14:15:48 -0600

    skeleton of project

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Robby Stigler on 10/29/19.
+//  Copyright © 2019 Robby Stigler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
